48bd0f5c78201c936d38418f0b1409398b33825e
Mattia Antonini <m.antonini@fbk.eu>
2023-02-19 15:29:17 +0100

    Rename Project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'segnaleticaSAT'`, () => {
+  it(`should have as title 'SegnaleticaSentieri'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('segnaleticaSAT');
+    expect(app.title).toEqual('SegnaleticaSentieri');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('segnaleticaSAT app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('SegnaleticaSentieri app is running!');
   });
 });
